4a2489e2a74e76a56c27fdbc7de56decf00ac411
Lucas Magalhães <lucasmagalhaes728@gmail.com>
2025-02-15 18:34:01 -0300

    feat: adicionado tela de dashboard, Implementado layout inicial com gráficos e métricas

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using MudBlazor.Services;
 using Big.Components;
 using Big.Data;
 using Big.Services;
@@ -11,19 +12,16 @@ using Big.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configurar a cultura padrão
 var culture = new CultureInfo("pt-BR");
 CultureInfo.DefaultThreadCurrentCulture = culture;
 CultureInfo.DefaultThreadCurrentUICulture = culture;
 
-// Configurar conexão com o banco de dados
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
                        ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-// Configurar Identity
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
     {
         options.Password.RequiredLength = 6;
@@ -33,7 +31,6 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
 
-// Configurar JWT Authentication
 var jwtKey = builder.Configuration["JwtSettings:Secret"] ?? "";
 var jwtIssuer = builder.Configuration["JwtSettings:Issuer"] ?? "BigApplication";
 
@@ -58,19 +55,19 @@ builder.Services.AddAuthentication(options =>
 
 builder.Services.AddAuthorization();
 
-// Configurar Razor Components e Serviços
+builder.Services.AddMudServices();
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddCascadingAuthenticationState();
+
 builder.Services.AddScoped<ProdutoService>();
 builder.Services.AddScoped<CategoriaService>();
 builder.Services.AddScoped<ClienteService>();
 builder.Services.AddScoped<PedidoService>();
 
-
 var app = builder.Build();
 
-// Configurar o pipeline de requisição
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -85,20 +82,17 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-app.UseAuthentication(); // Middleware de autenticação JWT
-app.UseAuthorization();  // Middleware de autorização
+app.UseAuthentication(); 
+app.UseAuthorization();  
 
-// Adicionar o middleware antifalsificação
 app.UseAntiforgery();
 
-// Mapear Razor Components
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
-// Seed de usuários e roles
 using (var scope = app.Services.CreateScope())
 {
     await IdentitySeeder.SeedRolesAndUsersAsync(scope.ServiceProvider);
 }
 
-app.Run(); 
+app.Run();
