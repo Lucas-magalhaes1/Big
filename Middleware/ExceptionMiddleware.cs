e17f5803b5b887db0fe9da4c7c610eb89afd7722
Lucas Magalhães <lucasmagalhaes728@gmail.com>
2025-02-21 22:26:42 -0300

    Middleware de Exceção

---
+++ Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Big.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Erro inesperado ocorreu.");
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsync("Ocorreu um erro interno no servidor.");
+            }
+        }
+    }
+}
